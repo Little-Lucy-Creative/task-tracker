2a31450f0e2b7db6dda180236c5148a32388c917
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-09-03 15:46:27 -0400

    addTask reducer

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,12 +2,11 @@ export const ADD_TASK = "ADD_TASK";
 export const DELETE_TASK = "DELETE_TASK";
 export const EDIT_TASK = "EDIT_TASK";
 
-export const addTask = taskName => {
-    return({type: ADD_TASK, payload: taskName});
+export const addTask = (list_id, task_name) => {
+    return({type: ADD_TASK, payload: {list_id: list_id, task_name: task_name});
 }
 export const deleteTask = id => {
     return({type: DELETE_TASK, payload: id });
 }
 export const editTask = (newTaskName, id) => {
-    return({type: EDIT_TASK, payload: {task: newTaskName, id: id })
-}
+    return({type: EDIT_TASK, payload: {newTaskName, id })
