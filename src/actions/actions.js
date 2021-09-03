d8d3a02b60e63eba04928e400646b2bdcb1f8559
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-09-03 15:50:39 -0400

    wip

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,10 +3,11 @@ export const DELETE_TASK = "DELETE_TASK";
 export const EDIT_TASK = "EDIT_TASK";
 
 export const addTask = (list_id, task_name) => {
-    return({type: ADD_TASK, payload: {list_id: list_id, task_name: task_name});
+    return({type: ADD_TASK, payload: {list_id: list_id, task_name: task_name}})
 }
 export const deleteTask = id => {
     return({type: DELETE_TASK, payload: id });
 }
 export const editTask = (newTaskName, id) => {
-    return({type: EDIT_TASK, payload: {newTaskName, id })
+    return({type: EDIT_TASK, payload: {newTaskName, id }})
+}
